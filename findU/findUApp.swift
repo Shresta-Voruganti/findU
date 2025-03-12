82034eac88715d81d8d4ece72a45bac444e9300c
Shresta-Voruganti <22bd1a053z@gmail.com>
2025-03-13 02:23:29 +0530

    Initial Commit

---
+++ findU/findUApp.swift
@@ -0,0 +1,17 @@
+//
+//  findUApp.swift
+//  findU
+//
+//  Created by SHRESTA VORUGANTI   on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct findUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
